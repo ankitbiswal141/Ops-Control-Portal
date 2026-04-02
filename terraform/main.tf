8c661f32723056719b94615e55317b2f9bb75718
Ankit Biswal <imankit.biswal43@gmail.com>
2026-04-02 08:28:17 +0000

    Terraform providers updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,33 @@ terraform {
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0" # Or your preferred version
+      version = ">= 1.14.0"
     }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
+}
+
+# Provider configurations
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
+provider "kubectl" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+  load_config_file       = false
 }
