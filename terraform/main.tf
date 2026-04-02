5a926f79253d1a5afeb76cba2cafde544275559c
Ankit Biswal <imankit.biswal43@gmail.com>
2026-04-02 10:46:04 +0000

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket141"
-    key            = "prod/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
