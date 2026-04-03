6a43120c255d3b4fc0ee6dcd1558d17bb358962f
Ankit Biswal <imankit.biswal43@gmail.com>
2026-04-03 05:04:09 +0000

    Data block added for eks cluster auth

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,7 @@
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
