6fb210ce74ef0758333c057a012452fdc65f62d9
Ankit Biswal <imankit.biswal43@gmail.com>
2026-04-03 04:53:18 +0000

    k8s secrets added and terraform output replaced

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,18 @@ output "argocd_loadbalancer_url" {
   value       = "http://${helm_release.argocd.metadata[0].name}.elb.amazonaws.com"
 }
 
-output "eks_cluster_name" {
+output "cluster_name" {
   value = module.eks.cluster_name
+}
+
+output "argocd_url" {
+  value = data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].hostname
+}
+
+data "kubernetes_service" "argocd_server" {
+  metadata {
+    name      = "argocd-server"
+    namespace = "argocd"
+  }
+  depends_on = [helm_release.argocd]
 }
